d416d00e68ae421a1d995a4fd32ca54ecd34ab1c
hosam al-deen abufasha <hosamabufashacr7@hotmail.com>
2023-07-25 14:14:53 +0300

    Initial Commit

---
+++ Qoutes Land/Qoutes_LandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qoutes_LandApp.swift
+//  Qoutes Land
+//
+//  Created by hosam abufasha on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Qoutes_LandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
